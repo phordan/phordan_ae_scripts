e7d5ac608ccfb32aede612e00ab9b1921a75d55a
Phillip Jordan <xjx388@gmail.com>
2024-07-11 16:53:33 -0500

    mid dataStruct refactor

--- Effect Explorer.jsx
+++ Effect Explorer.jsx
@@ -1,11 +1,11 @@
-// AE_EffectExplorer.jsx v1.0
+// Effect Explorer.jsx v0.9.0
 // Phillip Jordan 2024
 // Comissioned by Ingenuity Studios GFX Department
 // Based on original "AE_Effects_Checker.jsx" script by Rene Fabian Burgos at Ingenuity Studios
 
-// This script catalog effects used in the active project, and the composition(s) and layer(s) they are applied to.
+// This script catalogs effects used in the active project, and the composition(s) and layer(s) they are applied to.
 
-// Additions/Features:
+// Additions/Features (v0.9.0):
 // - convenient panel instead of individual alerts.
 // - displays the effects used in the project, grouped in a collabsible tree structure.
 // - option to use Effects or Comps as the top-level grouping
@@ -18,11 +18,13 @@
 //     - use effect's displayName or matchName
 //          - *when an effect uses multiple display names across the project (like if it was renamed), the matchName is always used*
 
-// - TODO features:
+// - TODO fix/features:
 //     - Finish "Group by Comp"
 //     - Finish Summary at bottom of panel, create summaryData object
 //     - Finish "item details" dialog
-//     - Edge cases (no comps selected, no effects in project/selected comps, etc.)
+//     - Edge case dialogs (no comps selected, no effects in project/selected comps, etc.)
+//     - spacing fixes
+//     - progress bar / noInterrupt on refresh
 //     - Sorting options
 //     - Consideration/warning of any nested comps' effects when applicable to prevent whoopsies on render
 //     - Filtering options like
@@ -43,12 +45,14 @@
 
 function effectsChecker(thisObj) {
         // declare anything used in multiple functions like data/controls/settings
-        var win, effectsTree, refreshButton, settingsButton;
+        var win, effectsTree, refreshButton, settingsButton, serarchStatusText, summaryText;
         var searchAllComps, searchSelectedComps, groupByEffect, groupByComp;
         var settingsPanel, useDisplayNameRadio, useMatchNameRadio, showDisabledCheckbox;
-        var projectData = {
+        var queryData = {
             effects: {},
             comps: {},
+            layers: {},
+            instances: []
         };
         var summaryData = {
             effectTotal: 0,
@@ -60,7 +64,7 @@ function effectsChecker(thisObj) {
             showDisabled: false
         };
         var scriptInfo = {
-            version: "1.0.0",
+            version: "0.9.0a",
             author: "Phillip Jordan",
             description: "Catalogs effects used in your project, and indicates the composition(s) and layer(s) they are applied to.",
         };
@@ -109,6 +113,8 @@ function effectsChecker(thisObj) {
         refreshButton.alignment = ["fill", "top"];
         settingsButton = buttonGroup.add("button", undefined, "Settings");
 
+        searchStatusText = catalogPanel.add("statictext", undefined, "");
+
         effectsTree = catalogPanel.add("treeview", undefined, []);
         effectsTree.alignment = ["fill", "fill"];
 
@@ -116,7 +122,7 @@ function effectsChecker(thisObj) {
         summaryGroup.orientation = "column";
         summaryGroup.alignment = ["fill", "bottom"];
         summaryGroup.alignChildren = ["fill", "center"];
-        summaryGroup.add("statictext", undefined, Object.keys(projectData.effects).length + " effects across " + Object.keys(projectData.comps).length + " comp(s)" +  "\r3737 total effect instances", {multiline: true});
+        summaryText = summaryGroup.add("statictext", undefined, Object.keys(queryData.effects).length + " effects across " + Object.keys(queryData.comps).length + " comp(s)" +  "\r3737 total effect instances", {multiline: true});
 
         settingsPanel = mainPanel.add("group");
         settingsPanel.orientation = "column";
@@ -171,6 +177,32 @@ function effectsChecker(thisObj) {
         }
     };
 
+    function ProgressBar(title) {
+        var win = new Window("palette", title, undefined, {closeButton: false});
+        var progressBar = win.add("progressbar", undefined, 0, 100);
+        progressBar.preferredSize.width = 300;
+        var statusText = win.add("statictext", undefined, "");
+        statusText.preferredSize.width = 300;
+
+        this.setTotal = function(total) {
+            progressBar.maxvalue = total;
+        };
+
+        this.update = function(value) {
+            progressBar.value = value;
+            statusText.text = "Processing: " + value + " / " + progressBar.maxvalue;
+            win.update();
+        };
+
+        this.show = function() {
+            win.show();
+        };
+
+        this.close = function() {
+            win.close();
+        };
+    };
+
     function updateDisplay() {
         effectsTree.removeAll();
         if (groupByEffect.value) {
@@ -180,26 +212,31 @@ function effectsChecker(thisObj) {
         }
     };
 
-    function collectAndDisplayEffects() {
-        projectData = {
-                effects: {},
-                comps: {}
-            };
-        summaryData = {
-                effectTotal: 0,
-                compTotal: 0,
-                effectInstanceTotal: 0
-            };
-        app.beginUndoGroup("Collect Effects");
-        
-        var compsToProcess = searchAllComps.value ? getAllComps() : getSelectedComps();
-        
-        for (var i = 0; i < compsToProcess.length; i++) {
-            processComposition(compsToProcess[i]);
+    function updateSearchStatus() {
+        var statusText = "";
+        var totalComps = getAllComps().length;
+        var searchedComps = searchAllComps.value ? totalComps : getSelectedComps().length;
+
+        if (searchAllComps.value) {
+            statusText = "Exploring Entire Project";
+        } else if (searchedComps === 1) {
+            statusText = "Exploring: Composition \"" + getSelectedComps()[0].name + "\"";
+        } else {
+            statusText = "Exploring " + searchedComps + " Selected Compositions";
         }
-        
-        app.endUndoGroup();
-        displayEffects();
+
+        searchStatusText.text = statusText;
+    };
+
+    function updateSummary() {
+        var effectCount = Object.keys(queryData.effects).length;
+        var compCount = Object.keys(queryData.comps).length;
+        var instanceCount = summaryData.effectInstanceTotal;
+        var summaryString = effectCount + (effectCount === 1 ? " effect" : " effects") + " across " +
+                        compCount + (compCount === 1 ? " comp" : " comps") + "\r" +
+                        instanceCount + " total effect instance" + (instanceCount === 1 ? "" : "s");
+
+        summaryText.text = summaryString;
     };
 
     function getAllComps() {
@@ -224,11 +261,50 @@ function effectsChecker(thisObj) {
         return selectedComps;
     };
 
+    function collectAndDisplayEffects() {
+        queryData = {
+                effects: {},
+                comps: {},
+                layers: {},
+                instances: []
+            };
+        summaryData = {
+                effectTotal: 0,
+                compTotal: 0,
+                effectInstanceTotal: 0
+            };
+
+        progressBar = new ProgressBar("Indexing Effects");
+        progressBar.show();
+        
+        app.beginUndoGroup("Collect Effects");
+        app.beginSupressDialogs();
+
+
+        try {
+            var compsToProcess = searchAllComps.value ? getAllComps() : getSelectedComps();
+            progressBar.setTotal(compsToProcess.length);
+            
+            for (var i = 0; i < compsToProcess.length; i++) {
+                processComposition(compsToProcess[i]);
+                progressBar.update(i+1);
+            }   
+        } finally {
+            app.endSupressDialogs(false);
+            app.endUndoGroup();
+            progressBar.close();
+        }    
+
+        updateSearchStatus();
+        displayEffects();
+        updateSummary();
+    };
+
     function processComposition(comp, parentCompName) {
         parentCompName = parentCompName || null; // This is how you do default params in extendscript
 
-        if (!projectData.comps[comp.name]) {
-            projectData.comps[comp.name] = {
+        if (!queryData.comps[comp.name]) {
+            queryData.comps[comp.name] = {
                 effects: {},
                 nestedComps: [],
                 totalEffectInstances: 0
@@ -239,7 +315,7 @@ function effectsChecker(thisObj) {
             var layer = comp.layer(i);
             if (layer.enabled) {
                 if (layer instanceof AVLayer && layer.source instanceof CompItem) {
-                    projectData.comps[comp.name].nestedComps.push(layer.source.name);
+                    queryData.comps[comp.name].nestedComps.push(layer.source.name);
                     processComposition(layer.source, comp.name);
                 } else {
                     processLayer(layer, comp.name);
@@ -247,9 +323,22 @@ function effectsChecker(thisObj) {
             }
         }
     };
+
+    function processLayer(layer, compName) {
+        var effects = layer.property("ADBE Effect Parade");
+        if (effects) {
+            for (var j = 1; j <= effects.numProperties; j++) {
+                var effect = effects.property(j);
+                addEffectData(effect, compName, layer.index + ": " + layer.name, layer.enabled && effect.enabled, j);
+            }
+        }
+    };
+
+    /*
     // Item info dialog that doesn't work yet
+
     function showEffectInfo(matchName) {
-        var effect = projectData.effects[matchName];
+        var effect = queryData.effects[matchName];
         var infoWindow = new Window("dialog", "Effect Info");
         infoWindow.orientation = "column";
         infoWindow.alignChildren = ["left", "top"];
@@ -261,83 +350,92 @@ function effectsChecker(thisObj) {
         var closeButton = infoWindow.add("button", undefined, "Close");
         closeButton.onClick = function() { infoWindow.close(); };
         infoWindow.show();
-    }
+    } */
 
-    function processLayer(layer, compName) {
-        var effects = layer.property("ADBE Effect Parade");
-        if (effects) {
-            for (var j = 1; j <= effects.numProperties; j++) {
-                var effect = effects.property(j);
-                addEffectData(effect, compName, layer.index + ": " + layer.name, layer.enabled && effect.enabled);
-            }
-        }
-    };
 
-    // Populate projectData with effect data while considering relations and updating counts
-    function addEffectData(effect, compName, layerName, isEnabled) {
+    // Populate queryData with effect data while considering relations and updating counts
+    function addEffectData(effect, compName, layerName, isEnabled, effectIndex) {
         var matchName = effect.matchName;
         var displayName = effect.name;
 
         // Update project-wide effect data
-        if (!projectData.effects[matchName]) {
-            projectData.effects[matchName] = {
+        if (!queryData.effects[matchName]) {
+            queryData.effects[matchName] = {
                 displayName: displayName,
                 totalInstances: 0,
                 enabledInstances: 0,
                 comps: {}
             };
         }
-        if (!projectData.effects[matchName].comps[compName]) {
-            projectData.effects[matchName].comps[compName] = {
+        if (!queryData.effects[matchName].comps[compName]) {
+            queryData.effects[matchName].comps[compName] = {
                 layers: {},
                 totalInstances: 0,
                 enabledInstances: 0
             };
         }
-        if (!projectData.effects[matchName].comps[compName].layers[layerName]) {
-            projectData.effects[matchName].comps[compName].layers[layerName] = {
-                instances: 0,
+        if (!queryData.effects[matchName].comps[compName].layers[layerName]) {
+            queryData.effects[matchName].comps[compName].layers[layerName] = {
+                instances: [],
                 enabledInstances: 0,
                 enabled: isEnabled
             };
         }
     
         // Update comp utilizing-layer and total instance counts per effect 
-        projectData.effects[matchName].totalInstances++;
-        projectData.effects[matchName].comps[compName].totalInstances++;
-        projectData.effects[matchName].comps[compName].layers[layerName].instances++;
+        queryData.effects[matchName].totalInstances++;
+        queryData.effects[matchName].comps[compName].totalInstances++;
+        queryData.effects[matchName].comps[compName].layers[layerName].instances.push({
+            index: effectIndex,
+            displayName: displayName,
+            enabled: isEnabled
+        });
 
         if (isEnabled) {
-            projectData.effects[matchName].enabledInstances++;
-            projectData.effects[matchName].comps[compName].enabledInstances++;
-            projectData.effects[matchName].comps[compName].layers[layerName].enabledInstances++;
+            queryData.effects[matchName].enabledInstances++;
+            queryData.effects[matchName].comps[compName].enabledInstances++;
+            queryData.effects[matchName].comps[compName].layers[layerName].enabledInstances++;
         }
 
         // Update comp-specific effect data
-        if (!projectData.comps[compName].effects[matchName]) {
-            projectData.comps[compName].effects[matchName] = {
+        if (!queryData.comps[compName].effects[matchName]) {
+            queryData.comps[compName].effects[matchName] = {
                 effects: {},
                 totalEffectInstances: 0,
                 enabledEffectInstances: 0
             };
         }
-        if (!projectData.comps[compName].effects[matchName]) {
-            projectData.comps[compName].effects[matchName] = {
+        if (!queryData.comps[compName].effects[matchName]) {
+            queryData.comps[compName].effects[matchName] = {
                 displayName: displayName,
                 layers: {},
                 totalInstances: 0,
                 enabledInstances: 0
             };
         }
+        if (!queryData.comps[compName].effects[matchName].layers[layerName]) {
+            queryData.comps[compName].effects[matchName].layers[layerName] = {
+                instances: [],
+                enabledInstances: 0,
+                enabled: isEnabled
+            };
+        }
 
         // Update comp's effect counts
-        projectData.comps[compName].effects[matchName].totalInstances++;
-        projectData.comps[compName].totalEffectInstances++;
+        queryData.comps[compName].effects[matchName].totalInstances++;
+        queryData.comps[compName].totalEffectInstances++;
+        queryData.comps[compName].effects[matchName].layers[layerName].instances.push({
+            index: effectIndex,
+            displayName: displayName,
+            enabled: isEnabled
+        });
 
         if (isEnabled) {
-            projectData.comps[compName].effects[matchName].enabledInstances++;
-            projectData.comps[compName].enabledEffectInstances++;
+            queryData.comps[compName].effects[matchName].enabledInstances++;
+            queryData.comps[compName].enabledEffectInstances++;
+            queryData.comps[compName].effects[matchName].layers[layerName].enabledInstances++;
         }
+        summaryData.effectInstanceTotal++;
     };
     
     // called by refresh button, calls correct tree rebuild function based on specified grouping
@@ -362,8 +460,8 @@ function effectsChecker(thisObj) {
 
     // Build the filtered effects tree using effect.matchName as the top-level grouping
     function displayEffectsByEffect() {
-        var effectsList = Object.keys(projectData.effects).map(function(matchName) {
-            return { matchName: matchName, effect: projectData.effects[matchName] };
+        var effectsList = Object.keys(queryData.effects).map(function(matchName) {
+            return { matchName: matchName, effect: queryData.effects[matchName] };
         });
 
         effectsList.sort(function(a, b) {
@@ -404,24 +502,31 @@ function effectsChecker(thisObj) {
                     var layerName = layerData.name;
                     var layerNodeData = layerData.data;
                     if (settings.showDisabled || layerNodeData.enabled) {
-                        var layerInstances = settings.showdisabled ? layerNodeData.instances : layerNodeData.enabledInstances;
-                        var layerItem = compNode.add("item", layerName + " (" + layerInstances + ")");
+                        var layerInstances = settings.showdisabled ? layerNodeData.instances.length : layerNodeData.enabledInstances;
+                        var layerNode = compNode.add("node", layerName + " (" + layerInstances + ")");
                         if (!layerNodeData.enabled) {
                             layerItem.enabled = false;
                         }
+                        layerNodeData.instances.forEach(function(instance) {
+                            if (settings.showdisabled || instance.enabled) {
+                                var instanceItem = layerNode.add("item", instance.index + ": " + instance.displayName);
+                                if (!instance.enabled) {
+                                    instanceItem.enabled = false;
+                                }
+                            }
+                        });
                     }
                 });
             });
 
-            effectNode.addEventListener('dblclick', function() { showEffectInfo(matchName); });
         });
     };
 
     // Build the filtered effects tree using comp.name as the top-level grouping
-    // Unfinished / broken; needs disabled logic and new count logic
+        // Unfinished / broken; needs disabled logic and new count logic
     function displayEffectsByComp() {
-        for (var compName in projectData.comps) {
-            var comp = projectData.comps[compName];
+        for (var compName in queryData.comps) {
+            var comp = queryData.comps[compName];
             compNode = effectsTree.add("node", compName + " (" + Object.keys(comp.effects).length = ") [" + comp.totalEffectInstances + "]");
 
             for (var matchName in comp.effects) {
